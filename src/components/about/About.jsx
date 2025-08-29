ee544c031493d8d6b7e06bba3e80e4d0540de4b6
Mari Myrmoen <mari.myr@gmail.com>
2025-08-29 15:02:31 +0200

    Implement card component with responsive layout, mobile carousel w swipe, expendable descr on mobile. Added projects data file

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,4 +1,12 @@
-import { FaHtml5, FaCss3Alt, FaJs, FaReact, FaNodeJs, FaFigma, FaGitAlt } from "react-icons/fa";
+import {
+  FaHtml5,
+  FaCss3Alt,
+  FaJs,
+  FaReact,
+  FaNodeJs,
+  FaFigma,
+  FaGitAlt,
+} from "react-icons/fa";
 import { useState } from "react";
 import "./about.css";
 
@@ -8,36 +16,49 @@ function About() {
   return (
     <section className="about">
       <section className="about__content">
-      <h3 className="about__title">About me</h3>
-      <p className="about__text">
-        Hi! I'm Mari, a Frontend Developer in the making, currently studying at
-        Folkuniversitetet in Gothenburg. During my first year, I gained
-        experience across a wide range of topics and technologies.
-      </p>
-      {showMore && (
-        <>
-          <p className="about__text">
-            This year, I am continuing to build on that foundation by exploring
-            cloud deployment, AI integration in modern applications, and
-            fullstack development with TypeScript. My skill set covers both
-            frontend and backend, with a strong focus on clean code,
-            user-friendly design, and agile collaboration. I enjoy creating
-            applications that are not only functional but also accessible and
-            visually engaging.
-          </p>
-        </>
-      )}
-      <button className="about__button" onClick={() => setShowMore(!showMore)}>
-        {showMore ? "Show less" : "Show more"}
-      </button>
-      <h3 className="about__subtitle">Tech Stack</h3>
-      <ul className="about__list">
-        <li><FaCss3Alt /> HTML & CSS <FaCss3Alt /></li>
-        <li><FaJs /> JavaScript / React <FaReact /></li>
-        <li><FaNodeJs /> Node.js / Express / MongoDB</li>
-        <li>UX / UI Design <FaFigma /> Figma</li>
-        <li><FaGitAlt /> Git, GitHub & Agile methods</li>
-      </ul>
+        <h3 className="about__title">About me</h3>
+        <p className="about__text">
+          Hi! I'm Mari, a Frontend Developer in the making, currently studying
+          at Folkuniversitetet in Gothenburg. During my first year, I gained
+          experience across a wide range of topics and technologies.
+        </p>
+        {showMore && (
+          <>
+            <p className="about__text">
+              This year, I am continuing to build on that foundation by
+              exploring cloud deployment, AI integration in modern applications,
+              and fullstack development with TypeScript. My skill set covers
+              both frontend and backend, with a strong focus on clean code,
+              user-friendly design, and agile collaboration. I enjoy creating
+              applications that are not only functional but also accessible and
+              visually engaging.
+            </p>
+            <h3 className="about__subtitle">Tech Stack</h3>
+            <ul className="about__list">
+              <li>
+                <FaCss3Alt /> HTML & CSS <FaCss3Alt />
+              </li>
+              <li>
+                <FaJs /> JavaScript / React <FaReact />
+              </li>
+              <li>
+                <FaNodeJs /> Node.js / Express / MongoDB
+              </li>
+              <li>
+                UX / UI Design <FaFigma /> Figma
+              </li>
+              <li>
+                <FaGitAlt /> Git, GitHub & Agile methods
+              </li>
+            </ul>
+          </>
+        )}
+        <button
+          className="about__button"
+          onClick={() => setShowMore(!showMore)}
+        >
+          {showMore ? "Show less" : "Show more"}
+        </button>
       </section>
     </section>
   );
